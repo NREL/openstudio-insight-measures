056bdc50f6f39d81e3ddfd9ec2d56f3d065fcc9f
Eric Ringold <ericringold@gmail.com>
2025-06-24 22:46:58 -0600

    update measure tests and test lib file

--- measures/replace_exterior_constructions_with_a_different_construction_from_resource_file/tests/replace_exterior_constructions_with_a_different_construction_from_resource_file_test.rb
+++ measures/replace_exterior_constructions_with_a_different_construction_from_resource_file/tests/replace_exterior_constructions_with_a_different_construction_from_resource_file_test.rb
@@ -11,7 +11,14 @@ class ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFileTest
   def test_arg_names
     measure = ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFile.new
 
-    path = OpenStudio::Path.new(File.expand_path(File.dirname(__FILE__) + '/../resources/Constructions.xml'))
+    # NOTE: measure tests use fake names defined in the test xml file. 
+    # when the measure is updated to use the production file, this test will not run
+    file_name = 'Constructions_test.xml'
+    if ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFile::LIB_FILE != file_name
+      skip "Test is using a different resource file: #{ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFile::LIB_FILE} != #{file_name}"
+    end
+    
+    path = OpenStudio::Path.new(File.expand_path(File.dirname(__FILE__) + "/../resources/#{file_name}"))
     xml_doc = File.read(path.to_s)
     parsed_xml = Oga.parse_xml(xml_doc)
     construction_names = parsed_xml.xpath("//Construction[@surfaceType='ExteriorWall' or @surfaceType='Roof']/Name").map(&:text)
@@ -23,11 +30,57 @@ class ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFileTest
     arg_choices = arguments_map['new_construction'].choiceValues
 
     assert_equal(arg_choices.size, construction_names.size)
+  end
 
+  def test_bad_argument
+    
+    # create an instance of the measure
+    measure = ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFile.new
+
+    # create runner with empty OSW
+    osw = OpenStudio::WorkflowJSON.new
+    runner = OpenStudio::Measure::OSRunner.new(osw)
+
+    # load the test model
+    translator = OpenStudio::OSVersion::VersionTranslator.new
+    path = "#{File.dirname(__FILE__)}/example_model.osm"
+    model = translator.loadModel(path)
+    assert(!model.empty?)
+    model = model.get
+
+    # get arguments
+    arguments = measure.arguments(model)
+    argument_map = OpenStudio::Measure.convertOSArgumentVectorToMap(arguments)
+
+    # create hash of argument values.
+    # If the argument has a default that you want to use, you don't need it in the hash
+    args_hash = {}
+    args_hash['new_construction'] = "Missing Construction Name"
+    args_hash['facade'] = 'North'
+
+    # populate argument with specified hash value if specified
+    # this will fail because the construction name is not in the resource file
+    # and the argument is required
+    arguments.each do |arg|
+      temp_arg_var = arg.clone
+      argument_map[arg.name] = temp_arg_var
+    end
+
+    # run the measure
+    measure.run(model, runner, argument_map)
+    result = runner.result
+
+    # show the output
+    show_output(result)
+
+    # assert that it ran correctly
+    assert_equal('Fail', result.value.valueName)
+    assert(result.errors.size == 1)
+    #assert(result.warnings.empty?)
   end
 
   def test_window_north
-    test_construction_name = 'Double glazing - 1/8 in thick - clear/low-E (e = 0.1) glass'
+    test_construction_name = 'Low-E double glazing (1/4 in + 1/4 in) U-1.98 SHGC-0.56'
 
     # create an instance of the measure
     measure = ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFile.new
@@ -89,14 +142,14 @@ class ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFileTest
       end
     end 
     north_windows.each do |window|
-      assert_equal(window.construction.get.name.get, test_construction_name + ' Construction')
+      assert_equal(window.construction.get.name.get, test_construction_name.split(' U-')[0].gsub(',',' |') + ' Construction')
     end
     non_north_windows.each do |window|
-      refute_equal(window.construction.get.name.get, test_construction_name)
+      refute_equal(window.construction.get.name.get, test_construction_name.split(' U-')[0].gsub(',',' |') + ' Construction')
     end
 
     # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}//output/test_output.osm"
+    output_file_path = "#{File.dirname(__FILE__)}//output/#{__method__}.osm"
     model.save(output_file_path, true)
   end
 
@@ -122,7 +175,7 @@ class ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFileTest
     # create hash of argument values.
     # If the argument has a default that you want to use, you don't need it in the hash
     args_hash = {}
-    args_hash['new_construction'] = 'Window Trp LoE'
+    args_hash['new_construction'] = 'Low-E double glazing (1/4 in + 1/4 in) U-1.98 SHGC-0.56'
     args_hash['facade'] = 'All'
     # using defaults values from measure.rb for other arguments
 
@@ -146,9 +199,17 @@ class ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFileTest
     assert_equal('Success', result.value.valueName)
     #assert(result.info.size == 1)
     #assert(result.warnings.empty?)
-
+    
+    # assert that all windows have the new construction
+    model.getSurfaces.each do |surf|
+      surf.subSurfaces.each do |sub_surf|
+        if sub_surf.subSurfaceType == 'FixedWindow'
+          assert_equal(sub_surf.construction.get.name.get, args_hash['new_construction'].split(' U-')[0].gsub(',',' |') + ' Construction')
+        end
+      end
+    end
     # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}//output/test_output.osm"
+    output_file_path = "#{File.dirname(__FILE__)}//output/#{__method__}.osm"
     model.save(output_file_path, true)
   end
 
@@ -174,7 +235,7 @@ class ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFileTest
     # create hash of argument values.
     # If the argument has a default that you want to use, you don't need it in the hash
     args_hash = {}
-    args_hash['new_construction'] = 'Wall R13+R10 Metal'
+    args_hash['new_construction'] = 'R-13+R13 metal frame + ins. panel (SIP) wall, 14\", 4 1/2 in (114 mm)'
     # using defaults values from measure.rb for other arguments
 
     # populate argument with specified hash value if specified
@@ -197,9 +258,15 @@ class ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFileTest
     assert_equal('Success', result.value.valueName)
     #assert(result.info.size == 1)
     #assert(result.warnings.empty?)
+    # assert that all exterior walls have the new construction
+    model.getSurfaces.each do |surf|
+      if surf.surfaceType == 'Wall' && surf.outsideBoundaryCondition == 'Outdoors'
+        assert_equal(surf.construction.get.name.get, args_hash['new_construction'].gsub(',',' |') + ' Construction')
+      end
+    end
 
     # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}//output/test_output.osm"
+    output_file_path = "#{File.dirname(__FILE__)}//output/#{__method__}.osm"
     model.save(output_file_path, true)
   end
 
@@ -225,7 +292,7 @@ class ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFileTest
     # create hash of argument values.
     # If the argument has a default that you want to use, you don't need it in the hash
     args_hash = {}
-    args_hash['new_construction'] = 'Roof 10.25-inch SIP'
+    args_hash['new_construction'] = 'Membrane, sheathing, R-15 insulation, 4 1/4 in light concrete'
     # using defaults values from measure.rb for other arguments
 
     # populate argument with specified hash value if specified
@@ -248,9 +315,14 @@ class ReplaceExteriorConstructionsWithADifferentConstructionFromResourceFileTest
     assert_equal('Success', result.value.valueName)
     #assert(result.info.size == 1)
     #assert(result.warnings.empty?)
+    model.getSurfaces.each do |surf|
+      if surf.surfaceType == 'RoofCeiling' && surf.outsideBoundaryCondition == 'Outdoors'
+        assert_equal(surf.construction.get.name.get, args_hash['new_construction'].gsub(',',' |') + ' Construction')
+      end
+    end
 
     # save the model to test output directory
-    output_file_path = "#{File.dirname(__FILE__)}//output/test_output.osm"
+    output_file_path = "#{File.dirname(__FILE__)}//output/#{__method__}.osm"
     model.save(output_file_path, true)
   end
 
